2369ff804cb17f25c65d731c70d578674579d808
tayo <opaseda@qa.com>
2021-04-01 11:21:51 +0000

    done

--- Jenkins-k8s/script.sh
+++ Jenkins-k8s/script.sh
@@ -4,6 +4,6 @@ cd Jenkins-k8s
 
 eksctl create cluster --name DemoCluster --region eu-west-2 --nodegroup-name DemoNodes --nodes 2 --nodes-min 2 --nodes-max 3 --node-type t3.medium --with-oidc --ssh-access --ssh-public-key terraformKey --managed
 
-kubctl apply -f mysql.yaml
+kubectl apply -f mysql.yaml
 
  
